643c83b8243c10a1985759206d19c8d49e34b876
MaC <mariocaster@gmail.com>
2011-08-11 11:08:04 +0200

    Agregado el resto de variables sin getter ni setters

--- gpx-parser/src/com/mariocaster/gie/TrackSegment.java
+++ gpx-parser/src/com/mariocaster/gie/TrackSegment.java
@@ -1,10 +1,24 @@
 package com.mariocaster.gie;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 
 public class TrackSegment extends ArrayList<TrackPoint> {
-	public float distancia;
-	public Date tiempo;
-
+	public double distancia;
+	public Calendar tiempo;
+	public int cadMedia;
+	public int frecMedia;
+	public float porcNegativoMedio;
+	public float porcPositivoMedio;
+	public float potenciaMedia;
+	public int cadenciaMaxima;
+	public int frecMaxima;
+	public double alturaMaxima;
+	public double alturaMinima;
+	public float rampaNegativaMaxima;
+	public float rampaPositivaMaxima;
+	public float potenciaMaxima;
+	public double ascensionTotal;
+	public double descensoTotal;
 }
