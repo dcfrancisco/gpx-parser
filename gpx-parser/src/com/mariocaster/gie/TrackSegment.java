4d39d3cb2a90481af2e6e70c5fa9ca65e9ed6c5f
MaC <mariocaster@gmail.com>
2011-08-11 10:46:40 +0200

    Quitados getters y setters para maximizar el rendimiento

--- gpx-parser/src/com/mariocaster/gie/TrackSegment.java
+++ gpx-parser/src/com/mariocaster/gie/TrackSegment.java
@@ -4,20 +4,7 @@ import java.util.Date;
 
 
 public class TrackSegment extends ArrayList<TrackPoint> {
-	private float distancia;
-	private Date tiempo;
-	
-	public float getDistancia() {
-		return distancia;
-	}
-	public Date getTiempo() {
-		return tiempo;
-	}
-	public void setDistancia(float distancia) {
-		this.distancia = distancia;
-	}
-	public void setTiempo(Date tiempo) {
-		this.tiempo = tiempo;
-	}
+	public float distancia;
+	public Date tiempo;
 
 }
